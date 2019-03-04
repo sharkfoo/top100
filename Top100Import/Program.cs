316321225ea98df10c09bb0d648d20014a948a92
Kevin Pearson <kevin.pearson@hp.com>
2019-03-04 15:13:30 -0700

    Pass csv filename in the arguments.

--- Top100Import/Program.cs
+++ Top100Import/Program.cs
@@ -2,6 +2,7 @@
 // © Copyright 2017 Kevin Pearson
 //
 
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Top100Common;
@@ -12,13 +13,20 @@ namespace Top100Import
     {
         public static async Task Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"\tUsage: {args[0]} <csv file>");
+                Console.WriteLine("");
+                return;
+            }
             var builder = new ConfigurationBuilder().AddEnvironmentVariables();
             var config = builder.Build();
             var mongoConnectionString = config["MONGO_CONNECTION_STRING"];
 
             var client = new Store(mongoConnectionString);
 
-            await Billboard100.ImportCSV(client, "top100.csv");
+            await Billboard100.ImportCSV(client, args[0]);
         }
     }
 }
